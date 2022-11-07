cb2b5a60973c0afbe189111f71945cea4968c6b2
valentinRog <valentin.rogister@gmail.com>
2022-11-07 20:07:56 +0100

    iterator_traits

--- src/iterator.hpp
+++ src/iterator.hpp
@@ -2,6 +2,16 @@
 
 namespace ft {
 
+/* --------------------------------- Traits --------------------------------- */
+
+template < typename T > struct iterator_traits {
+    typedef typename T::iterator_category iterator_category;
+    typedef typename T::value_type        value_type;
+    typedef typename T::difference_type   difference_type;
+    typedef typename T::pointer           pointer;
+    typedef typename T::reference         reference;
+};
+
 /* ---------------------------- Reverse iterator ---------------------------- */
 
 template < typename T > class reverse_iterator {
